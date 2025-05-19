e0677cb0eda276237085057850ef4ac0464ba422
Luís Carlos Costa Fonseca <t_luiscarlos@hapvida.com.br>
2025-05-19 18:00:11 -0300

    Primeira versão do código

---
+++ src/test/java/br/uema/mydelivery/MydeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uema.mydelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
