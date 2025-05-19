b0fc3dd7777bd4188c7e3276087db2b68192c366
Luís Carlos Costa Fonseca <t_luiscarlos@hapvida.com.br>
2025-05-19 18:17:55 -0300

    Entidade de Clientes

---
+++ src/main/java/br/uema/mydelivery/entity/Client.java
@@ -0,0 +1,31 @@
+package br.uema.mydelivery.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author lccf
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Client {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    Long cpf;
+    String email;
+    String celular;
+    Date created_at;
+    Date born_date;
+}
