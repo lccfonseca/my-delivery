372bb5ddb912e4e9c79c98cb1d341b59025cb25d
Luís Carlos Costa Fonseca <t_luiscarlos@hapvida.com.br>
2025-05-29 18:30:30 -0300

    Inclusão do web-app

--- src/main/java/br/uema/mydelivery/MydeliveryApplication.java
+++ src/main/java/br/uema/mydelivery/MydeliveryApplication.java
@@ -1,13 +1,36 @@
 package br.uema.mydelivery;
 
+import java.util.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class MydeliveryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MydeliveryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MydeliveryApplication.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean simpleCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("localhost"));
+        config.setAllowedOriginPatterns(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setExposedHeaders(Collections.singletonList("Access-Control-Expose-Headers: *"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
 
 }
